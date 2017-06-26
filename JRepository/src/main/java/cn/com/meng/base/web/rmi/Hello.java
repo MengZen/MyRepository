4569f777330e1ab40073352aef33eb7c52b53178
MengInBM <zhengdongxing@bmsoft.com.cn>
2017-06-26 10:49:31 +0800

    增加rmi方法demo

---
+++ JRepository/src/main/java/cn/com/meng/base/web/rmi/Hello.java
@@ -0,0 +1,32 @@
+package cn.com.meng.base.web.rmi;
+import java.rmi.*;    
+import java.rmi.server.*;    
+   
+/**   
+ * 扩展了UnicastRemoteObject类，并实现远程接口 HelloInterface   
+ */   
+public class Hello extends UnicastRemoteObject implements HelloInterface    
+{    
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = -3953818040410256107L;
+private String message;    
+   
+   /**   
+    * 必须定义构造方法，即使是默认构造方法，也必须把它明确地写出来，因为它必须抛出出RemoteException异常   
+    */   
+   public Hello(String msg) throws RemoteException    
+   {    
+      message = msg;    
+   }    
+   
+   /**   
+    * 远程接口方法的实现   
+    */   
+   public String say() throws RemoteException    
+   {    
+      System.out.println("Called by HelloClient");    
+      return message;    
+   }    
+}
---
+++ JRepository/src/main/java/cn/com/meng/base/web/rmi/HelloInterface.java
@@ -0,0 +1,13 @@
+package cn.com.meng.base.web.rmi;
+import java.rmi.*;    
+   
+/**   
+ * 远程接口必须扩展接口java.rmi.Remote   
+ */   
+public interface HelloInterface extends Remote    
+{    
+   /**   
+    * 远程接口方法必须抛出 java.rmi.RemoteException   
+    */   
+   public String say() throws RemoteException;    
+}
